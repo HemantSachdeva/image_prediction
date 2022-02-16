027a3494d668bb574e5855b6e2813cdc6aa7df0f
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-02-16 20:35:51 +0530

    Make prediction speed fast
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- src/image_prediction.py
+++ src/image_prediction.py
@@ -8,7 +8,7 @@ prediction = ImageClassification()
 prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(
     execution_path, "assets/binaries/mobilenet_v2.h5"))
-prediction.loadModel()
+prediction.loadModel(classification_speed="fast")
 
 predictions, probabilities = prediction.classifyImage(
     os.path.join(execution_path, "assets/images/car.jpg"), result_count=5)
