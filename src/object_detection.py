5beac76ab2fa46bb952d37a3cfc08c107c3adadc
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-02-17 13:28:00 +0530

    Extract each detected object in output image path
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- src/object_detection.py
+++ src/object_detection.py
@@ -9,10 +9,12 @@ detector.setModelTypeAsTinyYOLOv3()
 detector.setModelPath(os.path.join(
     execution_path, "assets/binaries/yolo-tiny.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(
-    execution_path, "assets/images/family.jpg"), output_image_path=os.path.join(execution_path, "assets/images/family-detected.jpg"), minimum_percentage_probability=30)
+detections, objects_path = detector.detectObjectsFromImage(input_image=os.path.join(
+    execution_path, "assets/images/family.jpg"), output_image_path=os.path.join(execution_path, "assets/images/family-detected.jpg"),
+    minimum_percentage_probability=30, extract_detected_objects=True)
 
-for eachObject in detections:
+for eachObject, eachObjectPath in zip(detections, objects_path):
     print(eachObject.get("name"), " : ", eachObject.get("percentage_probability"),
           " : ", eachObject.get("box_points"))
+    print("Object's image saved in " + eachObjectPath)
     print("--------------------------------")
